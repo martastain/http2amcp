4d9ef09908839aac4806860138ac7fd9a294398f
Martin Wacker <martas@imm.cz>
2021-02-02 04:12:41 -0800

    Initial working version

--- http2amcp.py
+++ http2amcp.py
@@ -5,21 +5,25 @@ from vial import Vial
 from nxtools import logging
 from nxtools.caspar import CasparCG
 
-
-caspar = CasparCG("localhost")
-
+caspar = CasparCG("127.0.0.1")
+caspar.query("VERSION")
 
 class App(Vial):
     def handle(self, request):
-        if request.method == "POST":
+        if request.method == "POST" and request.path == "/amcp":
+            r = caspar.query(request.body.text)
 
-            return self.response.text()
+            return self.response.text(
+                r.data, 
+                status=r.response,
+                custom_headers={
+                    "Access-Control-Allow-Origin" : "*"
+                }
+            )
 
-
-        return self.response.text(f"BAAAAAD", status=400)
+        return self.response.text(f"Bad request", status=400)
 
 app = App(logger=logging)
 
 if __name__ == "__main__":
-    app.serve("", 8000)
-
+    app.serve("127.0.0.1", 9731)
